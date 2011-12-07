6c344e06c1954a247de256471239fed3e4f25ee2
Bastiaan Roos <bastiaan.roos@nelen-schuurmans.nl>
2011-12-07 14:30:28 +0100

    bugfix

--- lizard_esf/api/views.py
+++ lizard_esf/api/views.py
@@ -101,7 +101,7 @@ class ConfigurationTreeView(View):
     """
     def get(self, request):
 
-        area = self.CONTENT.get('object_id', None)
+        area =  request.GET.get('object_id', None)
         area = Area.objects.get(ident=area)
 
         area_config = AreaConfiguration.objects.filter(area=area)
