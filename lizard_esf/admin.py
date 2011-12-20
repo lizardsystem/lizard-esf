3400eb54d913ff2bff10d581a12fc4e4e7c77647
Bastiaan Roos <bastiaan.roos@nelen-schuurmans.nl>
2011-12-20 09:33:12 +0100

    extra display field in admin

--- lizard_esf/admin.py
+++ lizard_esf/admin.py
@@ -7,7 +7,7 @@ from lizard_esf.models import ConfigurationType
 
 
 class ConfigurationAdmin(admin.ModelAdmin):
-    list_display = ('name', 'value_type')
+    list_display = ('name', 'path', 'depth', 'manual', 'value_type')
 
 
 admin.site.register(Configuration, ConfigurationAdmin)
