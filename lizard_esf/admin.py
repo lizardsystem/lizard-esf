a1adffdc11732153010df5ca8aeee8943c920348
Bastiaan Roos <bastiaan.roos@nelen-schuurmans.nl>
2012-03-15 00:09:58 -0700

    some admin changes for comfiguration of ESF

--- lizard_esf/admin.py
+++ lizard_esf/admin.py
@@ -12,43 +12,44 @@ class ConfigurationAdmin(admin.ModelAdmin):
     list_display = (
         'id',
         'name',
-        'code',
+        #'code',
         'path',
         'depth',
-        # 'manual',
+        'manual',
         'configuration_type',
         'value_type',
-        # 'default_parameter_code_manual_fews',
-        # 'timeserie_ref_status',
+        'default_parameter_code_manual_fews',
+        'timeserie_ref_status',
         # 'dbf_file',
-        'dbf_index',
-        # 'dbf_valuefield_name',
-        # 'dbf_valuefield_type',
-        # 'dbf_valuefield_length',
-        # 'dbf_valuefield_decimals',
-        # 'dbf_manualfield_name'
+        #'dbf_index',
+        'dbf_valuefield_name',
+        #'dbf_valuefield_type',
+        #'dbf_valuefield_length',
+        #'dbf_valuefield_decimals',
+        'dbf_manualfield_name'
     )
 
     list_editable = (
         'name',
-        'code',
+        #'code',
         'path',
         'depth',
-        # 'manual',
+        'manual',
         'configuration_type',
         'value_type',
-        # 'default_parameter_code_manual_fews',
-        # 'timeserie_ref_status',
+        'default_parameter_code_manual_fews',
+        'timeserie_ref_status',
         # 'dbf_file',
-        'dbf_index',
-        # 'dbf_valuefield_name',
-        # 'dbf_valuefield_type',
-        # 'dbf_valuefield_length',
-        # 'dbf_valuefield_decimals',
-        # 'dbf_manualfield_name'
+        #'dbf_index',
+        'dbf_valuefield_name',
+        #'dbf_valuefield_type',
+        #'dbf_valuefield_length',
+        #'dbf_valuefield_decimals',
+        'dbf_manualfield_name'
     )
 
     list_filter = ('dbf_file',)
+    ordering = ('path',)
 
 
 admin.site.register(Configuration, ConfigurationAdmin)
